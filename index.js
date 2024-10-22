daa1d8cb071d4d0448371740c65c79276a06535e
PoornaShreeP <nischayhr2004@gmail.com>
2024-10-23 04:31:09 +0530

    Added All the Project Implementation

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const app=express();
+const port=8080;
+const path=require("path"); 
+
+app.set("view engine","ejs");                                    // When The Response Is 'Rendered' default path to access.
+app.set("views",path.join(__dirname,"/views"));               
+app.use(express.static(path.join(__dirname,"/public")));         // Default middleware : for default paths.
+app.use(express.urlencoded({extended:true}));    
+
+app.listen(port,(req,res)=>{
+    console.log("Server Started..");
+});
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+
+app.get("/email",(req,res)=>{
+    res.render("email.ejs");
+});
+
+app.get("/scan",(req,res)=>{
+    res.render("scan.ejs");
+});
+
+app.post("/scaning",(req,res)=>{
+    res.render("scaning.ejs");
+});
+
+app.post("/report",(req,res)=>{
+    res.render("report.ejs");
+});
+
+app.post("/it",(req,res)=>{
+    res.render("it.ejs");
+})
